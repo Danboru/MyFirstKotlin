97c4817baa3046905cda2b84443a5488557e08dc
Danboru <8danang8@gmail.com>
2017-05-26 07:28:19 +0700

    Stable version

--- app/src/main/java/id/eightstudio/danboru/myfirstkotlin/MainActivity.kt
+++ app/src/main/java/id/eightstudio/danboru/myfirstkotlin/MainActivity.kt
@@ -1,70 +1,79 @@
 package id.eightstudio.danboru.myfirstkotlin
 
-import android.content.Context
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity(){
 
-    override fun onCreate(savedState: Bundle?) {
-        super.onCreate(savedState)
-        setContentView(R.layout.activity_list_view)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
 
-        val lv = findViewById(R.id.list) as ListView
-        lv.adapter = ListExampleAdapter(this)
-    }
+        val edt1 = findViewById(R.id.edt_satu) as EditText
+        val edt2 = findViewById(R.id.edt_dua) as EditText
+        val result = findViewById(R.id.tv_result) as TextView
+        val btnTambah = findViewById(R.id.btn_tambah) as Button
+        val btnKurang = findViewById(R.id.btn_kurang) as Button
+        val btnKali = findViewById(R.id.btn_kali) as Button
+        val btnBagi = findViewById(R.id.btn_bagi) as Button
 
-    private class ListExampleAdapter(context: Context) : BaseAdapter() {
+        btnTambah.setOnClickListener {
 
-        internal var sList = arrayOf("One", "Two", "Three", "Four", "Five", "Six", "Seven",
-                "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen")
+            val a: String = edt1.text.toString()
+            val b: String = edt2.text.toString()
 
-        private val mInflator: LayoutInflater
-
-        init {
-            this.mInflator = LayoutInflater.from(context)
-        }
-
-        override fun getCount(): Int {
-            return sList.size
-        }
-
-        override fun getItem(position: Int): Any {
-            return sList[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
-            val view: View?
-            val vh: ListRowHolder
-            if (convertView == null) {
-                view = this.mInflator.inflate(R.layout.list_row, parent, false)
-                vh = ListRowHolder(view)
-                view.tag = vh
-
+            if(edt1.text.isEmpty() && edt2.text.isEmpty()){
+                Toast.makeText(applicationContext,"silahkan isi", Toast.LENGTH_SHORT).show()
             } else {
-                view = convertView
-                vh = view.tag as ListRowHolder
+                val c = a.toInt()
+                val d = b.toInt()
+                val e = c + d
+                result.setText(e.toString())
             }
+        }
 
-            vh.label.text = sList[position]
-            return view
+        btnKurang.setOnClickListener {
+            v ->  kurang()
+        }
+
+        btnKali.setOnClickListener {
+            v -> result.setText(kali(edt1.text.toString().toInt(), edt2.text.toString().toInt()).toString())
+        }
+
+        btnBagi.setOnClickListener{
+            v -> result.text = (getEdt1() / getEdt2()).toString()
         }
     }
 
-    private class ListRowHolder(row: View?) {
-        public val label: TextView
-
-        init {
-            this.label = row?.findViewById(R.id.label) as TextView
+    fun kurang() {
+        val edt1 = findViewById(R.id.edt_satu) as EditText
+        val edt2 = findViewById(R.id.edt_dua) as EditText
+        val result = findViewById(R.id.tv_result) as TextView
+        if(edt1.text.isEmpty() || edt2.text.isEmpty()){
+            Toast.makeText(applicationContext,"silahkan isi", Toast.LENGTH_SHORT).show()
+        } else {
+            val a: Int = edt1.text.toString().toInt()
+            val b: Int = edt2.text.toString().toInt()
+            val c: Int = a - b
+            result.setText(c.toString())
         }
+    }
+
+    fun kali(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun getEdt1(): Int {
+        val edt1 = findViewById(R.id.edt_satu) as EditText
+        val a: String = edt1.text.toString()
+        return a.toInt()
+    }
+
+    fun getEdt2(): Int {
+        val edt2 = findViewById(R.id.edt_dua) as EditText
+        val b: String = edt2.text.toString()
+        return b.toInt()
     }
 
 }
